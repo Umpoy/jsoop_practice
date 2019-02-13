9487868371b891da1047801b083336966c58cb0d
Umpoy <iumpoy@yahoo.com>
2019-02-12 18:46:54 -0800

    now holding numbers in array

--- exercises/basiccalcobject.js
+++ exercises/basiccalcobject.js
@@ -1,30 +1,45 @@
 
-class Calculator{
-	constructor(){
+class Calculator {
+	constructor() {
 		//takes in nothing
 		//make storage for the operator and the numbers
+		this.numbers = []
+
 	}
-	loadOperator(  ){
+	loadOperator(operator) {
 		//adds the operator to the next calculation
 		//takes in the operator
 		//checks if it is a valid operation (+-*/)
-			//save the op to the constructor storage
-			//return true
+		//save the op to the constructor storage
+		//return true
 		//or return false if not the right operator
+		if (operator !== '+' || '-' || '*' || '/') {
+			return false
+		}
+		return operator
+
 	}
-	loadNumber(  ){
+	loadNumber(number) {
 		//takes in a number and stores it as one of the numbers to perform math on
 		//takes in 1 number
 		//checks if it is actually a number and if we have fewer than 2 numbers
 		//if it is a number, and we have 2 or fewer numbers, store it
-			//return the number of numbers stored
+		//return the number of numbers stored
 		//otherwise return false (too many numbers stored)
+		if (typeof number !== 'number') {
+			return false
+		}
+		if (this.numbers.length < 2) {
+			this.numbers.push(number);
+		}
+		return this.numbers.length
 	}
-	calculate(){
+	calculate() {
 		//calculate the result of the stored numbers and operator
 		//takes in nothing
 		//calculates with the operator and 2 numbers
 		//clears out the stored numbers
 		//returns the calculated result
+
 	}
 }
