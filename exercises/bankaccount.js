ad155d1a2b36ebe39d8b3bb8e4fc89d8ceeca281
Umpoy <iumpoy@yahoo.com>
2019-02-12 19:19:45 -0800

    featureset 3

--- exercises/bankaccount.js
+++ exercises/bankaccount.js
@@ -1,23 +1,37 @@
 
 
-class Account{
-	constructor(){
+class Account {
+	constructor() {
 		//store the amount of money in the account
+		this.amount = 0;
 	}
-	add(  ){
+	add(amount) {
 		//add money to the amount stored in the account
 		//takes in an amount
 		//adds it to the existing amount
 		//returns the new amount in the account
+		this.amount += amount
+		return this.amount
 	}
-	remove( amount ){
+	remove(amount) {
 		//removes funds from an account
 		//check if the amount to withdraw is more than the account
 		//if more, only withdraw the amount in the account, not more
 		//if less, withdraw the amount specified
 		//return the amount actually withdrawn
+		let withdraw;
+		if (amount > this.amount) {
+			withdraw = this.amount;
+			this.amount = 0
+		} else {
+			withdraw = amount;
+			this.amount -= amount
+		}
+
+		return withdraw
 	}
-	getAmount(){
+	getAmount() {
 		//returns the amount in the account
+		return this.amount
 	}
 }
